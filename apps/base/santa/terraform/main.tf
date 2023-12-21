2901e9d41a4000715ccb3371703ecc15a737ec6f
Robin Smorenburg <robin@robino.io>
2023-12-21 12:13:53 +0100

    Add vars

--- apps/base/santa/terraform/main.tf
+++ apps/base/santa/terraform/main.tf
@@ -22,8 +22,22 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+locals {
+  # Set the application name
+  app = "santa"
+
+  # Lookup and set the location abbreviation, defaults to na (not available).
+  location_abbreviation = try(var.location_abbreviation[var.location], "na")
+
+  # Lookup and set the environment abbreviation, defaults to na (not available).
+  environment_abbreviation = try(var.environment_abbreviation[var.environment], "na")
+
+  # Construct the name suffix.
+  suffix = "${local.app}-${local.environment_abbreviation}-${local.location_abbreviation}"
+}
+
 # Create the resource group.
 resource "azurerm_resource_group" "default" {
-  name     = "rg-santa-${var.environment}"
-  location = "northeurope"
+  name     = "rg-${local.suffix}"
+  location = var.location
 }
--- apps/base/santa/terraform/variables.tf
+++ apps/base/santa/terraform/variables.tf
@@ -1,4 +1,37 @@
+variable "app" {
+  description = "Required. The name of the application."
+  type        = string
+}
+
+variable "location" {
+  description = "Required. The location (region) for the resources."
+  type        = string
+}
+
+variable "location_abbreviation" {
+  description = "Optional. The abbreviation of the location."
+  type        = map(string)
+  default     = {
+    "westeurope"  = "weu"
+    "northeurope" = "neu"
+    "eastus"      = "eus"
+    "westus"      = "wus"
+    "ukwest"      = "ukw"
+    "uksouth"     = "uks"
+  }
+}
+
 variable "environment" {
   description = "Required. The environment for the resources."
   type        = string
+}
+
+variable "environment_abbreviation" {
+  description = "Optional. The abbreviation of the environment."
+  type        = map(string)
+  default     = {
+    "development" = "dev"
+    "staging"     = "stage"
+    "production"  = "prod"
+  }
 }
