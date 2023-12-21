2bee0e3aecf1b72b0dcc93e4c3508b6c266a7878
Robin Smorenburg <robin@robino.io>
2023-12-21 12:41:43 +0100

    Add vars

--- apps/base/santa/terraform/main.tf
+++ apps/base/santa/terraform/main.tf
@@ -7,12 +7,6 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id      = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
-  tenant_id            = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
-  client_id            = "223b377c-bb9b-499c-9c9e-106b95d7c628"
-  use_oidc             = true
-  oidc_token_file_path = "/var/run/secrets/azure/tokens/azure-identity-token"
-
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
