5e55ae3dd379bb0f7a4e946d2ae7d16aa0cdfe94
Robin Smorenburg <robin@robino.io>
2023-12-26 14:19:00 +0100

    Remove MongoDB configuration in Terraform file
    The MongoDB-related configurations have been removed from the main Terraform file. This includes features like automatic failover, defined capabilities and consistency policies. It also includes geo_location settings. Thus, settings related to the use of MongoDB are no longer enforced in this Terraform configuration.

--- apps/base/santa/terraform/main.tf
+++ apps/base/santa/terraform/main.tf
@@ -50,36 +50,6 @@ resource "azurerm_cosmosdb_account" "default" {
   location            = var.location
   resource_group_name = azurerm_resource_group.default.name
   offer_type          = "Standard"
-  kind                = "MongoDB"
-
-  enable_automatic_failover = true
-
-  capabilities {
-    name = "EnableAggregationPipeline"
-  }
-
-  capabilities {
-    name = "mongoEnableDocLevelTTL"
-  }
-
-  capabilities {
-    name = "MongoDBv3.4"
-  }
-
-  capabilities {
-    name = "EnableMongo"
-  }
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-
-  geo_location {
-    location          = "westeurope"
-    failover_priority = 0
-  }
 }
 
 # Create the Kubernetes namespace.
