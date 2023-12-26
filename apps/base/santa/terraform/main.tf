d716bcd51db5df96610bf4337e7bce9d5b690d32
Robin Smorenburg <robin@robino.io>
2023-12-26 14:20:04 +0100

    Enable automatic failover in terraform configuration
    The commit introduces automatic failover in the main.tf terraform configuration file for the `santa` application. This includes setting up a consistency policy and defining a geo_location for failover. Failover priority has also been set and the consistency level has been defined as "BoundedStaleness".

--- apps/base/santa/terraform/main.tf
+++ apps/base/santa/terraform/main.tf
@@ -50,6 +50,19 @@ resource "azurerm_cosmosdb_account" "default" {
   location            = var.location
   resource_group_name = azurerm_resource_group.default.name
   offer_type          = "Standard"
+
+  enable_automatic_failover = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "westeurope"
+    failover_priority = 0
+  }
 }
 
 # Create the Kubernetes namespace.
