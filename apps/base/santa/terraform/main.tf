0cc243fe4b87745257eb1008e1b78f7e7cd622b1
Robin Smorenburg <robin@robino.io>
2023-12-28 17:21:16 +0100

    Add Terraform infrastructure code for 'santa' application
    This commit introduces Terraform code for setting up 'santa' application. The infrastructure includes resources for Azure, Kubernetes and CosmosDB. The resources are specified for both staging and production environments. RBAC credentials are used for authentication with Azure.

---
+++ apps/base/santa/terraform/main.tf
@@ -0,0 +1,253 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = ">= 3.84"
+    }
+
+    random = {
+      version = ">= 3.6"
+    }
+
+    kubernetes = {
+      version = ">= 2.24"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+locals {
+  # Lookup and set the location abbreviation, defaults to na (not available).
+  location_abbreviation = try(var.location_abbreviation[var.location], "na")
+
+  # Lookup and set the environment abbreviation, defaults to na (not available).
+  environment_abbreviation = try(var.environment_abbreviation[var.environment], "na")
+
+  # Construct the name suffix.
+  suffix = "${var.app}-${local.environment_abbreviation}-${local.location_abbreviation}"
+}
+
+# Generate a random suffix for the CosmosDB account.
+resource "random_id" "cosmosdb" {
+  byte_length = 3
+}
+
+# Create the resource group.
+resource "azurerm_resource_group" "default" {
+  name     = "rg-${local.suffix}"
+  location = var.location
+}
+
+# Create the CosmosDB account, database, and container.
+resource "azurerm_cosmosdb_account" "default" {
+  name                      = "cosmos-${var.app}-${local.environment_abbreviation}-${random_id.cosmosdb.hex}"
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.default.name
+  offer_type                = "Standard"
+  enable_automatic_failover = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "northeurope"
+    failover_priority = 0
+  }
+
+  geo_location {
+    location          = "westeurope"
+    failover_priority = 1
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "default" {
+  name                = "ftc23"
+  resource_group_name = azurerm_resource_group.default.name
+  account_name        = azurerm_cosmosdb_account.default.name
+
+  autoscale_settings {
+    max_throughput = "1000"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "default" {
+  name                = "persons"
+  resource_group_name = azurerm_resource_group.default.name
+  account_name        = azurerm_cosmosdb_account.default.name
+  database_name       = azurerm_cosmosdb_sql_database.default.name
+  partition_key_path  = "/LastName"
+
+  autoscale_settings {
+    max_throughput = "1000"
+  }
+}
+
+# Create the Kubernetes namespace.
+resource "kubernetes_namespace_v1" "default" {
+  metadata {
+    name = var.app
+
+    labels = {
+      app = var.app
+    }
+  }
+}
+
+# Create the Kubernetes deployment.
+resource "kubernetes_deployment_v1" "default" {
+  metadata {
+    name      = var.app
+    namespace = kubernetes_namespace_v1.default.metadata[0].name
+
+    labels = {
+      app = var.app
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = var.app
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app
+        }
+      }
+
+      spec {
+        container {
+          image = var.image
+          name  = var.app
+
+          port {
+            container_port = 3000
+            protocol       = "TCP"
+          }
+
+          env {
+            name  = "NEXT_PUBLIC_COSMOS_DB_ENDPOINT"
+            value = azurerm_cosmosdb_account.default.endpoint
+          }
+
+          env {
+            name  = "NEXT_PUBLIC_COSMOS_DB_KEY"
+            value = azurerm_cosmosdb_account.default.primary_key
+          }
+
+          env {
+            name  = "NEXT_PUBLIC_COSMOS_DB_CONTAINER_ID"
+            value = "persons"
+          }
+
+          env {
+            name  = "NEXT_PUBLIC_COSMOS_DB_DATABASE_ID"
+            value = "ftc23"
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 3000
+            }
+
+            initial_delay_seconds = 10
+          }
+        }
+      }
+    }
+  }
+}
+
+# Create the Kubernetes service.
+resource "kubernetes_service_v1" "default" {
+  metadata {
+    name      = var.app
+    namespace = kubernetes_namespace_v1.default.metadata[0].name
+
+    labels = {
+      app = var.app
+    }
+  }
+
+  spec {
+    selector = {
+      app = var.app
+    }
+
+    port {
+      port        = 3000
+      target_port = 3000
+    }
+  }
+}
+
+# Create the ingress.
+resource "kubernetes_ingress_v1" "default" {
+  metadata {
+    name      = var.app
+    namespace = kubernetes_namespace_v1.default.metadata[0].name
+
+    labels = {
+      app = var.app
+    }
+
+    annotations = {
+      "cert-manager.io/cluster-issuer" = "letsencrypt"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = [var.ingress_rule_host]
+      secret_name = "tls-secret"
+    }
+
+    rule {
+      host = var.ingress_rule_host
+
+      http {
+        path {
+          backend {
+            service {
+              name = var.app
+              port {
+                number = 3000
+              }
+            }
+          }
+
+          path      = "/"
+          path_type = "Prefix"
+        }
+      }
+    }
+  }
+}
---
+++ apps/base/santa/terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "app" {
+  description = "Optional. The name of the application."
+  type        = string
+  default     = "santa"
+}
+
+variable "location" {
+  description = "Optional. The location (region) for the resources."
+  type        = string
+  default     = "northeurope"
+}
+
+variable "location_abbreviation" {
+  description = "Optional. The abbreviation of the location."
+  type        = map(string)
+  default     = {
+    "westeurope"  = "weu"
+    "northeurope" = "neu"
+    "eastus"      = "eus"
+    "westus"      = "wus"
+    "ukwest"      = "ukw"
+    "uksouth"     = "uks"
+  }
+}
+
+variable "environment" {
+  description = "Required. The environment for the resources."
+  type        = string
+}
+
+variable "environment_abbreviation" {
+  description = "Optional. The abbreviation of the environment."
+  type        = map(string)
+  default     = {
+    "development" = "dev"
+    "staging"     = "stage"
+    "production"  = "prod"
+  }
+}
+
+variable "image" {
+  description = "Required. The image for the container."
+  type        = string
+}
+
+variable "ingress_rule_host" {
+  description = "Required. The host for the ingress rule."
+  type        = string
+}
